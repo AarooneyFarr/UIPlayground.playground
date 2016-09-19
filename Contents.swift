ccf6a8019b4eae5ada8af5201f983a9b8c2e8ed1
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-19 09:53:55 -0600

    Initial setup

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var DynamicView=UIView(frame: CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.greenColor()
+DynamicView.layer.cornerRadius=25
+DynamicView.layer.borderWidth=2
+self.view.addSubview(DynamicView)
+
+
+
+
+
+
